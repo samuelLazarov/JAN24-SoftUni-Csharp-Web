7e3c170d88698cb0bd89b0919747a32e69f8bd9b
SamuelLazarov <samuel.lazarov@gmail.com>
2024-01-22 12:34:09 +0200

    ASP.NET

---
+++ MVCIntroDemo/MVCIntroDemo/Controllers/NumbersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCIntroDemo.Controllers
+{
+	public class NumbersController : Controller
+	{
+		private readonly ILogger logger;
+
+        public NumbersController(ILogger<NumbersController> _logger)
+        {
+            logger = _logger;
+        }
+        public IActionResult Index()
+		{
+			return View(50);
+		}
+
+		[HttpGet]
+		public IActionResult Limit(int num)
+		{
+			return View("Index", num);
+		}
+	}
+}
---
+++ MVCIntroDemo/MVCIntroDemo/wwwroot/js/numbers.js
@@ -0,0 +1,5 @@
+﻿function SetLimit() {
+    let num = document.getElementById("limitInput").value || 50;
+
+    window.location = "https://localhost:7092/Numbers/Limit?num=" + num;
+}
