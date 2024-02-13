08384e96dbfa1ea0f69363d310e9684cce200ba0
SamuelLazarov <samuel.lazarov@gmail.com>
2024-02-13 19:05:17 +0200

    Added Models

---
+++ ExamPreparation/Homies/Controllers/EventController.cs
@@ -0,0 +1,99 @@
+﻿using Homies.Data;
+using Homies.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Homies.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly HomiesDbContext data;
+
+        public EventController(HomiesDbContext context)
+        {
+            data = context;
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var events = await data.Events
+                .AsNoTracking()
+                .Select(e => new EventInfoViewModel(
+                e.Id,
+                e.Name,
+                e.Start,
+                e.Type.Name,
+                e.Organiser.UserName
+                   
+                ))
+                .ToListAsync();
+
+            return View(events);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Join(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Joined()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Leave(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(EventFormViewModel model)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EventFormViewModel model, int id)
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            return View();
+        }
+
+
+        public string GetUserId()
+        {
+            return User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? string.Empty;
+        }
+
+        private async Task<IEnumerable<TypeViewModel>> GetTypes()
+        {
+            return await data.Types
+                .AsNoTracking()
+                .Select(t => new TypeViewModel
+                {
+                    Id = t.Id,
+                    Name = t.Name
+                })
+                .ToListAsync();
+        }
+    }
+}
